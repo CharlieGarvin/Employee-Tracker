8f3a3e3f4aa45597f0571f79972994a6d34b43ea
unknown <charliegarvin90@gmail.com>
2023-04-30 15:15:44 -0600

    added start function in the index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
-const connections = mysql.createConnection({
+
+const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
@@ -9,3 +10,39 @@ const connections = mysql.createConnection({
     database: 'employees_db'
 });
 
+function start() {
+    inquirer.createPrompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choice: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Update employee role', 'Exit'],
+    }).then(function (answer) {
+        switch(answer.action) {
+            case 'View all departments':
+                viewDepartment();
+                break;
+            case 'View all roles':
+                viewRoles();
+                break;
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Update employee role':
+                updateEmployeeRole();
+                break;
+            case 'Exit':
+                connection.end();
+                break;
+               default:
+                break;         
+        }
+    }); 
+} 
+
+start ();
